901a6d7a85966fab6811af3050c64a9497897a89
victorxvtz <victorxvtz@gmail.com>
2023-02-07 15:30:48 +0100

    graph assignment complete

--- Week1/001.House/src/House.java
+++ Week1/001.House/src/House.java
@@ -8,7 +8,7 @@ import org.jfree.fx.FXGraphics2D;
 public class House extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
+        Canvas canvas = new Canvas(960, 540);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("House");
@@ -17,6 +17,28 @@ public class House extends Application {
 
 
     public void draw(FXGraphics2D graphics) {
+        //walls
+        graphics.drawLine(100, 100,100,150);
+        graphics.drawLine(200,150, 200, 100);
+        graphics.drawLine(300, 85, 300, 135);
+        //roof
+        graphics.drawLine(100,100,150, 50);
+        graphics.drawLine(150, 50, 200, 100);
+        graphics.drawLine(150, 50, 250, 35);
+        graphics.drawLine(200, 100, 300, 85);
+        graphics.drawLine(250, 35, 300, 85);
+        //floor
+        graphics.drawLine(100,150,200, 150);
+        graphics.drawLine(200, 150, 300, 135);
+        //door
+        graphics.drawLine(115, 115, 135, 115);
+        graphics.drawLine(115, 150,115,115);
+        graphics.drawLine(135, 115, 135,150);
+        //window
+        graphics.drawLine(145, 115, 145, 135);
+        graphics.drawLine(185, 115, 185, 135);
+        graphics.drawLine(145, 115, 185,115);
+        graphics.drawLine(145, 135, 185, 135);
     }
 
 
--- Week1/002.Graph/src/Graph.java
+++ Week1/002.Graph/src/Graph.java
@@ -9,21 +9,43 @@ import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 
 public class Graph extends Application {
+    Canvas canvas = new Canvas(960, 540);
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Canvas canvas = new Canvas(1920, 1080);
         draw(new FXGraphics2D(canvas.getGraphicsContext2D()));
         primaryStage.setScene(new Scene(new Group(canvas)));
         primaryStage.setTitle("Graph");
         primaryStage.show();
+
     }
-    
-    
+
+
     public void draw(FXGraphics2D graphics) {
+
+        graphics.translate(this.canvas.getWidth()/2, this.canvas.getHeight()/2);
+        graphics.scale( 1, -0.5);
+
+        graphics.setColor(Color.red);
+        graphics.drawLine(0,0,1000,0);
+        graphics.setColor(Color.green);
+        graphics.drawLine(0,0,0,1000);
+        graphics.setColor(Color.black);
+
+        double resolution = 0.1;
+        double lastY = Math.pow(-10, 3);
+
+        for(double x = -10; x < 10; x += resolution)
+        {
+            float y = (float)Math.pow(x, 3);
+            graphics.draw(new Line2D.Double(x, y, x-resolution, lastY));
+            lastY = y;
+        }
     }
-    
-    
-    
+
+
+
+
     public static void main(String[] args) {
         launch(Graph.class);
     }
